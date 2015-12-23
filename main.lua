7d1e9292422afbc489f435df772984422d6f529e
Ant Cosentino <antony@mycosentino.com>
2015-12-23 13:41:31 +0200

    added some more callbacks for rendering to main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,16 @@
 
-function love.draw()
+-- your game here
+
+debug = true
+
+function love.load(arg)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw(dt)
   love.graphics.print("Hello World!", 400, 300)
 end
